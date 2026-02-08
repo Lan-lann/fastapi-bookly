2549e0b780f67fa18b58868b0a8980a670324272
Lanlann <2293697325@qq.com>
2026-02-08 19:20:20 +0800

    add email verification

--- src/middleware.py
+++ src/middleware.py
@@ -36,5 +36,5 @@ def register_middleware(app: FastAPI):
 
     app.add_middleware(
         TrustedHostMiddleware,
-        allowed_hosts=["“localhost", "127.0.0.1"]
+        allowed_hosts=["localhost", "127.0.0.1"]
     )
